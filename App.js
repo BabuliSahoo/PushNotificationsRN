6be082a677b624c7c575652d81736acb47bdba0d
Babuli Sahoo <babuli.sahoo4@gmail.com>
2024-06-01 00:20:52 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Button, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler(
+  {
+    handleNotification: async () =>{
+      console.log('setNotificationHandler 123 called :');
+      return {
+        shouldPlaySound:false,
+        shouldSetBadge:false,
+        shouldShowAlert:true
+      };
+    }
+  }
+);
 
 export default function App() {
+  function scheduleNotificationHandler() {
+    console.log('scheduleNotificationHandler called : ');
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "My first Local Notification",
+        body: "Body of the Notifications",
+        data:{userName:'Babuli'}
+      },
+      trigger:{
+        seconds: 5
+      }
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Schedule Notification"
+        onPress={scheduleNotificationHandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#94d3a6",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
